6ff9304bd87ba3f1e129806fb1bba6b19ba84dc2
Ashwani <imakumar98@gmail.com>
2020-02-04 10:55:08 +0530

    bubble sort complete

--- sorting/bubble-sort.cpp
+++ sorting/bubble-sort.cpp
@@ -5,6 +5,36 @@ using namespace std;
 
 int main() {
 
-	cout<<"Hello world";
-	return 0;
+	int arr[5] = {8, 7, 6, 4, 3};
+
+	int length = 5;
+
+	int swaps = 0;
+
+	for(int i=0; i<length-1; i++) {
+
+		for(int j=0; j<length-1; j++){
+			if(arr[j]>arr[j+1]) {
+				
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+
+			} 
+		}
+
+
+	}
+
+	//Print Array
+	for(int i=0; i<length; i++) {
+
+		cout<<arr[i]<<" ";
+	}
+
+
+	cout<<"\n"<<"Number of swaps : "<<swaps;
+
+	
 }
